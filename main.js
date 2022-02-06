ec12e5009dc1bb16bcbc644e88b18f380c59cd72
Henrique Klug <henriqueklug@gmail.com>
2022-02-06 00:04:57 -0300

    v1

--- main.js
+++ main.js
@@ -1,15 +1,24 @@
 import { API_KEY, BASE_URL, IMG_URL, language } from './api.js'
 
 const Movie = {
-  number: Math.random() * (995 - 2) + 2,
 
   getData(movieNum) {
-    fetch(BASE_URL + movieNum + '?api_key=' + API_KEY)
+    fetch(BASE_URL + movieNum + '?api_key=' + API_KEY + '&' + language)
       .then(response => response.json())
       .then(data => {
-        movieTitle.textContent = data.original_title
-        movieDescription.textContent = data.overview
-        moviePoster.src = IMG_URL + data.poster_path
+        if (data.adult == true || data.overview == "" || data.poster_path == null) {
+          this.getData(parseInt(Math.random() * (715000 - 1) + 1))
+        } else {
+          movieTitle.textContent = data.original_title
+          movieDescription.textContent = data.overview
+          moviePoster.src = IMG_URL + data.poster_path
+        }
       })
   }
 }
+
+document.querySelector('#shuffle-button')
+  .addEventListener('click', () => {
+    let randomNumber = parseInt(Math.random() * (715000 - 1) + 1)
+    Movie.getData(randomNumber);
+  })
